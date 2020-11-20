7b53e8f97d7d8e327568d57a529466a9a4d2a836
motty93 <rdwbocungelt5@gmail.com>
2020-11-20 14:02:33 +0900

    update: app.tsx json

--- App.tsx
+++ App.tsx
@@ -1,21 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { Image, StyleSheet } from 'react-native'
+import { HomeScreen } from './src/screens/HomeScreen'
+import { LandingScreen } from './src/screens/LandingScreen'
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { createAppContainer, createSwitchNavigator } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
+import { createBottomTabNavigator } from 'react-navigation-tabs'
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const switchNavigator = createSwitchNavigator({
+
+  landingStack: {
+    screen: createStackNavigator({
+      landing: LandingScreen,
+    }, {
+      defaultNavigationOptions: {
+        headerShown: false
+      }
+    })
   },
-});
+  homeStack: createBottomTabNavigator({
+    // Home tab icon
+    home: {
+      screen: createStackNavigator({
+        HomePage: LandingScreen
+      }),
+      navigationOptions: {
+        tabBarIcon: ({ focused, tintColor }) => {
+          let name = focused == true ? 'home' : 'home-work'
+          return <Icon name={name} size={30} color={tintColor} />
+        }
+      }
+    },
+
+    Offer: {
+      screen: createStackNavigator({
+        OfferPage: HomeScreen
+      }),
+      navigationOptions: {
+        tabBarIcon: ({ focused, tintColor }) => {
+          let name = focused == true ? 'local-offer' : 'local-offer'
+          return <Icon name={name} size={30} color={tintColor} />
+        }
+      }
+    },
+
+    Cart: {
+      screen: createStackNavigator({
+        CartPage: HomeScreen
+      }),
+      navigationOptions: {
+        tabBarIcon: ({ focused, tintColor }) => {
+          let name = focused == true ? 'shopping-cart' : 'add-shopping-cart'
+          return <Icon name={name} size={30} color={tintColor} />
+        }
+      }
+    },
+
+    Account: {
+      screen: createStackNavigator({
+        AccountPage: HomeScreen
+      }),
+      navigationOptions: {
+        tabBarIcon: ({ focused, tintColor }) => {
+          let name = focused == true ? 'account-box' : 'account-circle'
+          return <Icon name={name} size={30} color={tintColor} />
+        }
+      }
+    }
+  })
+})
+
+const AppNaviagtion = createAppContainer(switchNavigator)
+
+const App: React.FC = () => (
+  // <LandingScreen />
+  <AppNaviagtion />
+)
+
+export default App
